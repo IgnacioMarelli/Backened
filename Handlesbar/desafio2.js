1f177bf40f0bb55f80bfc2e2ac5c84e1a5e07dea
IgnacioMarelli <ignacio.marelli@hotmail.com>
2022-07-20 13:48:51 -0300

    desafio

---
+++ Handlesbar/desafio2.js
@@ -0,0 +1,98 @@
+const fs = require('fs');
+class Contenedor{
+    constructor(archivo){
+        this.archivo=archivo;
+    }
+    async save(product){
+        try{
+            const data = await this.getAll();
+            product.id = data.length + 1;
+            data.push(product);
+            fs.writeFileSync(this.archivo, JSON.stringify(data));
+            return {
+                product: product,
+            };
+        }catch (error){
+            console.log(`El error es: ${error}`);
+        }
+    }
+
+    async getById(id){
+        try{
+            const respuesta = await fs.promises.readFile(this.archivo, "utf-8");
+            const datos = JSON.parse(respuesta);
+            const resultado = datos.findIndex((obj)=> obj.id == id);
+            if (resultado > 0) {
+                return (datos[resultado])
+            } else {
+                return (null)
+            }
+        }catch(error){
+            console.log(`El error es: ${error}`);
+        }
+    }
+    async update(id, product) {
+		const data = await this.getAll();
+		if (id <= 0 || id > data.length) {
+			return {
+				error: "El producto con el id especificado no ha sido encontrado.",
+			};
+		}
+		product.id = id;
+		const previousProduct = data.splice(id - 1, 1, product);
+		fs.writeFileSync(this.archivo, JSON.stringify(data));
+		return {
+			anterior: previousProduct,
+			nuevo: product,
+		};
+	}
+
+    async getAll(){
+        try {
+            const respuesta = await fs.promises.readFile(this.archivo, "utf-8");
+            const productos = JSON.parse(respuesta);
+            return  productos
+        } catch (error) {
+            console.log(`El error es: ${error}`);
+        }
+    }
+    async getRandomItem(){
+        try {
+            const respuesta = await fs.promises.readFile(this.archivo, "utf-8");
+            const productos = JSON.parse(respuesta);
+            console.log(productos)
+            const productoRandom= productos[parseInt(Math.random()*3)];
+            return productoRandom;
+        } catch (error) {
+            console.log(`El error es: ${error}`);
+        }
+    }
+
+     deleteById(id){
+        try {
+            const respuesta =  fs.readFile(this.archivo, "utf-8");
+            const productosParseados = JSON.parse(respuesta);
+            const prods = productosParseados.filter(prod=> prod.id!==id);
+             fs.writeFile(this.archivo, JSON.stringify(prods));
+            return `Has eliminado el producto con id: ${id} de la lista`
+        } catch (error) {
+            console.log(`El error es: ${error}`);
+        }
+    }
+    async deleteAll (){
+        try {
+            const respuesta = await fs.promises.readFile(this.archivo, "utf-8");
+            if (respuesta == "") {
+                console.log('No hay productos para borrar');
+            } else {
+                await fs.promises.writeFile(this.archivo, "");
+            }
+        } catch (error) {
+            console.log(`El error es: ${error}`);
+        }
+    }
+}
+
+const contenedor1 = new Contenedor("productos.txt");
+
+module.exports = { Contenedor }
--- Pug/src/server.js
+++ Pug/src/server.js
@@ -3,45 +3,34 @@ const exphbs = require('express-handlebars');
 const app = express();
 const { Router }= express;
 const router = Router();
-
-const { Contenedor }= require ('../desafio2');
+const { Contenedor }= require ('../../Handlesbar/desafio2');
 const contenedor1 = new Contenedor("productos.json");
 app.use('/api', router);
 router.use(express.json());
 router.use(express.urlencoded({extended:true}));
 let prodsAgregados = [];
+app.use(express.static("public"));
+app.set("view engine", ".pug");
+app.set("views", "views");
 
-app.engine(
-    ".hbs",
-    exphbs.engine({
-        extname: ".hbs",
-        defaultLayout:"main.hbs",
-        layoutsDir: "./views/layout"
-    })
-);
-
-
-app.set('views', './views');
-app.set("view engine", ".hbs");
 
 router.get('/', (req, res)=>{
-    res.render('../views/formulario.hbs');
+    res.render('../views/formulario');
 })
 router.post('/productos', (req, res)=>{
     const { title, price, thumbnail } = req.body;
     const productoAgregado= { title, price, thumbnail };
-    
     if (title === "" || price === "" || thumbnail === "") {
-        res.render("../views/faltanDatos");
+        res.render("../views/faltanDatos", {prodsAgregados});
     }else{
-     prodsAgregados.push(productoAgregado);
-     res.render('../views/historial', {productoAgregado});
+        prodsAgregados.push(productoAgregado);
+        res.render('../views/historial', {prodsAgregados});
     }
 })
 
-router.get('/productos', (req,res)=>{
-    res.render('../views/productos', {prodsAgregados})
-})
+
+
+
 const PORT = process.env.PORT || 8080;
 const server = app.listen(PORT, ()=>{
     console.log(`Servidor corriendo en el puerto ${PORT}`);
